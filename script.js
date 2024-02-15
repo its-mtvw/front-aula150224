ba96c51b9a0b6296f7dcc819f8539401b17033c0
its-mtvw <mauriciowerven@gmail.com>
2024-02-15 18:49:03 +0000

    g

---
+++ script.js
@@ -0,0 +1,15 @@
+const app=Vue.createApp(
+    {
+        data()
+        {
+            return{
+                productName: "Cruise ship",
+                productSubtitle: "Subtitle of product",
+                productDescription: "Cruise ship description"
+                
+
+            }
+        }
+    }
+)
+app.mount('#app')
